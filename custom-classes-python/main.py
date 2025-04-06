bb43810a2b5af657ed2e079d0b562268419a6209
RabinSharma25 <cruzosharmas22545@gmail.com>
2025-04-06 22:07:39 +0530

    added the custom class code

---
+++ custom-classes-python/main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from rectangle import Rectangle
+
+# Create an instance of the Rectangle class
+rect = Rectangle(10, 5)
+
+# Iterate over the instance and print each value
+for item in rect:
+    print(item)
+
+
+# to execute this file 
+# -> python3 main.py
